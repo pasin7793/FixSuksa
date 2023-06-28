72c8502d733151ebdb8c6d67a51855e66d465ec4
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2023-06-28 09:16:41 +0900

    Initial Commit

---
+++ FixSuksa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FixSuksa
+//
+//  Created by 임준화 on 2023/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
